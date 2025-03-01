ef00da0a49a6ab9e97ad9843256bd4b594f2bca4
Zaharov Andrey <zaharov.andrey161@gmail.com>
2025-03-01 13:24:42 +0300

    Added choosing db and file path.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from app.gui import get_gui
 
 
 def main():
-    worker = SQLiteWorker("db.sqlite3")
-    worker.insert_data_from_json("data.json")
+    worker = SQLiteWorker(input("Enter db path (*.sqlite3):"))
+    worker.insert_data_from_json(input("Enter .json file path:"))
 
 
 if __name__ == "__main__":
